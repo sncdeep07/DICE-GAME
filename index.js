59e012b618aedda0cf64685b695d307dc5b2c559
sncdeep07 <96005571+sncdeep07@users.noreply.github.com>
2021-12-14 08:52:14 +0530

    Edit index.js & give functionality to <button>
    edited index.js file incorporating comments and giving functionality to button element, so user doesn't have to refresh the browser again & again to play the game.

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
-let randomNum1 = Math.floor(Math.random() * 6) + 1;
+// Declaring variables
+let randomNum1;
 const img1 = document.querySelector(".img1");
-img1.setAttribute("src", `images/dice${randomNum1}.png`);
 
-let randomNum2 = Math.floor(Math.random() * 6) + 1;
+let randomNum2;
 const img2 = document.querySelector(".img2");
-img2.setAttribute("src", `images/dice${randomNum2}.png`);
 
 const title = document.querySelector("h1");
+const btn = document.querySelector(".btn");
 
-if (randomNum1 > randomNum2) {
-  title.textContent = "🚩Player 1 Wins!";
-} else if (randomNum2 > randomNum1) {
-  title.textContent = "Player 2 Wins!🚩";
-} else {
-  title.textContent = "Draw!";
+// Call function for initial dice roll or when user resfresh browser
+rollDice();
+
+//Add click event to button element
+btn.addEventListener("click", () => {
+  // call function when user click the button
+  rollDice();
+});
+
+// Main function to roll the dice and give the result output
+function rollDice() {
+  randomNum1 = Math.floor(Math.random() * 6) + 1;
+  img1.setAttribute("src", `images/dice${randomNum1}.png`);
+
+  randomNum2 = Math.floor(Math.random() * 6) + 1;
+  img2.setAttribute("src", `images/dice${randomNum2}.png`);
+
+  if (randomNum1 > randomNum2) {
+    title.textContent = "🚩Player 1 Wins!";
+  } else if (randomNum2 > randomNum1) {
+    title.textContent = "Player 2 Wins!🚩";
+  } else {
+    title.textContent = "Draw!";
+  }
 }
